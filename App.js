3881c6b4639b2eada90e67606118d49c20258d6a
Aldo Arias <aldo.arias@hardrockdigital.com>
2021-07-11 20:18:52 -0500

    adding UI componentes to the App.js and creation of Card

--- App.js
+++ App.js
@@ -1,17 +1,61 @@
 import React from 'react';
-import {Text} from 'react-native';
 import styled from 'styled-components';
+import Card from './components/Card';
 
 const Container = styled.View`
   background: #f0f3f5;
   flex: 1;
 `;
 
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 20px;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const Subtitle = styled.Text`
+  font-size: 15px;
+  font-weight: 600;
+  color: #b8bece;
+  margin-top: 50px;
+  margin-left: 20px;
+  text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  background: black;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
 export default class App extends React.Component {
   render() {
     return (
       <Container>
-        <Text>ooaldo</Text>
+        <TitleBar>
+          <Title>welcome</Title>
+          <Name>aldo</Name>
+          <Avatar source={require('./assets/avatar.jpg')} />
+          <Subtitle>continue learning</Subtitle>
+        </TitleBar>
+        <Card />
       </Container>
     );
   }
---
+++ components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.View`
+  width: 315px;
+  height: 280px;
+  border-radius: 14px;
+  background-color: white;
+  margin-top: 20px;
+  margin-left: 20px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+`;
+
+const Card = porps => <Container />;
+
+export default Card;
